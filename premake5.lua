dd270c0574b7afca078e156a529d5029f1651b59
klen-list <anton.burkov.02@mail.ru>
2022-01-21 09:10:31 +0300

    add default submodules path to premake file

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,18 @@
 PROJECT_GENERATOR_VERSION = 3
 
 newoption {
-	trigger = "gmcommon",
-	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
-	value = "path to garrysmod_common directory"
-} 
+    trigger = "gmcommon",
+    description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+    value = "path to garrysmod_common directory"
+}
 
-gmcommon = _OPTIONS.gmcommon or os.getenv "GARRYSMOD_COMMON"
-assert(gmcommon, "You didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory.")
+local gmcommon = _OPTIONS.gmcommon or os.getenv "GARRYSMOD_COMMON"
 
-include(gmcommon)
+if not gmcommon then
+	print "You didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory, using submodule directory instead."
+end
+
+include(gmcommon or "third-party/garrysmod_common")
 
 CreateWorkspace { name = "network" }
 	CreateProject { serverside = true }
